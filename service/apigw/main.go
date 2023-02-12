f1010709e44a1c484d21ffbdc60c4b7a6fccfcc5
guagualu <2534066116@qq.com>
2023-02-12 20:59:15 +0800

    second

--- service/apigw/main.go
+++ service/apigw/main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	http.HandleFunc("/user/signup", route.UserSignup)
 	http.HandleFunc("/user/signin", route.UserSignin)
+	http.HandleFunc("/user/userinfo", route.UserInfoHandler)
+	http.HandleFunc("/user/getfilemeta", route.GetFileMetaHandler)
+	http.HandleFunc("/user/filequery", route.FileQueryHandler)
+	http.HandleFunc("/user/filerename", route.FileRenameHandler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("listen server err:", err)
